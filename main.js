22d3f8d7610d07aa90b5a3d40144f7876d5e4f58
wsfmaw15 <127457500+wsfmaw15@users.noreply.github.com>
2023-11-16 20:24:13 -0300

    Update main.js
    16112023

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+      document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = Document.querySelectorAll('.tecla');
+
+//para
+for(let contador = 0; contador <listaDeTeclas.length;contador++) {
+
+   const tecla = listaDeTeclas{contador};
+   const instrumento = tecla.ClassList[1];
+  const idAudio = `#som_${instrumento}`; //template string
+
+tecla.onclick = function () {
+     tocaSom(idAudio);
 }
 
-document.querySelector('.tecla')
+tecla.onkeydown = function (evento) {
+    
+    console.log(evento.cod == 'Space')
+
+    if(evento.code == 'Space'){
+    tecla.ClassList.add('ativa');
+    
+}
+tecla.onkeyup = function() {
+    tecla.ClassList.remove('ativa');
+
+ 
+}
